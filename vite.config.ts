f6a29081ff812871c79c4b70cdbe40bf4c58d9b9
Dubabbi <thdms3160@naver.com>
2025-09-24 09:29:58 +0900

    Chore: 필요 없는 패키지 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      iconDirs: [path.resolve(process.cwd(), 'src/shared/assets/icons')],
       symbolId: 'icon-[name]',
     }),
   ],
