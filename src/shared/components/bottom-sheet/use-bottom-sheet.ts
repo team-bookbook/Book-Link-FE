f6f510d0c644a9f754b8f5acc9745b3c910ded8f
Dubabbi <thdms3160@naver.com>
2025-09-27 22:55:01 +0900

    Feat: 바텀시트 훅 구현

---
+++ src/shared/components/bottom-sheet/use-bottom-sheet.ts
@@ -0,0 +1,13 @@
+import { useCallback, useState } from 'react';
+
+const useBottomSheet = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
+  const toggle = useCallback(() => setIsOpen((prev) => !prev), []);
+
+  return { isOpen, open, close, toggle };
+};
+
+export default useBottomSheet;
