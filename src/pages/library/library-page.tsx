451cb81038e58939303db95ccc8ee5cc4b8b1837
changchangwoo <changchangwoo@naver.com>
2025-10-23 18:29:28 +0900

    Feat: useLocalStorage 공통 훅 추가

--- src/pages/library/library-page.tsx
+++ src/pages/library/library-page.tsx
@@ -2,12 +2,13 @@ import LibraryTab, { type LibraryTabKey } from '@pages/library/components/librar
 import { useQueryTab } from '@hooks/use-query-tab';
 import LibraryList from '@pages/library/components/section/section-librarylist';
 import BookList from '@pages/library/components/section/section-booklist';
-import { useState } from 'react';
+import { useLocalStorage } from '@hooks/use-local-storage';
 import { useKakaoMaps } from '@hooks/use-kakao-map';
+import type { TLocation } from './types/library.types';
 
 export default function LibraryPage() {
   const [tab, setTab] = useQueryTab<LibraryTabKey>('tab', 'books', ['books', 'libraries']);
-  const [location, setLocation] = useState({
+  const [location, setLocation] = useLocalStorage<TLocation>('library-location', {
     address: '서울 마포구 효창원로98길 1-1',
     lat: 37.48486731057572,
     lng: 126.92841740891708,
